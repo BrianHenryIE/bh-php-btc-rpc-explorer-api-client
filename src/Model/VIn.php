af966b2b2a501ab897c506241d026607a85ac66a
Brian Henry <BrianHenryIE@gmail.com>
2025-12-29 23:51:28 -0800

    `txIinWitness`

--- src/Model/VIn.php
+++ src/Model/VIn.php
@@ -16,7 +16,7 @@ readonly class VIn
 {
     /**
      * @param string|null $coinbase Coinbase data - arbitrary data included by miners in the first transaction of a block. Only present in coinbase transactions where new bitcoin is created.
-     * @param string[] $txinwitness Transaction input witness - SegWit witness data containing signatures and public keys, stored separately from legacy transaction data.
+     * @param string[] $txIinWitness Transaction input witness - SegWit witness data containing signatures and public keys, stored separately from legacy transaction data.
      * @param int $sequence Sequence number - used for transaction replacement and relative timelocks. Value of 0xffffffff indicates finalized transaction.
      * @param string|null $txId Transaction ID (txid) - unique identifier of the transaction containing the output being spent. Null for coinbase transactions.
      * @param int|null $vOut Output index (vout) - the position of the output in the referenced transaction's output array. Null for coinbase transactions.
@@ -24,7 +24,8 @@ readonly class VIn
      */
     public function __construct(
         public ?string $coinbase = null,
-        public array $txinwitness = [],
+        #[MapFrom('txinwitness')]
+        public array $txIinWitness = [],
         public int $sequence = 0,
         #[MapFrom('txid')]
         public ?string $txId = null,
